e744019b5b93bd4e3b8da5c5592fb1d9f4076626
Marat <sayfullin97@gmail.com>
2024-06-11 11:22:02 +0500

    Добавлен файл .gitignore и игнорируем папки node_modules и build

---
+++ gulpfile.mjs
@@ -0,0 +1,104 @@
+import fs from 'fs';
+
+async function clean() {
+  await fs.promises.rm('build', { recursive: true, force: true });
+}
+ 
+
+
+import gulp from 'gulp';
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import concat from 'gulp-concat';
+import terser from 'gulp-terser';
+import cleanCSS from 'gulp-clean-css';
+import preprocess from 'gulp-preprocess';
+import rename from 'gulp-rename';
+import browserSync from 'browser-sync';
+
+const sass = gulpSass(dartSass);
+const bs = browserSync.create();
+
+const paths = {
+  html: 'src/**/*.html',
+  sass: 'src/sass/**/*.sass',
+  js: 'src/js/**/*.js',
+  img: 'src/img/**/*',
+  fonts: 'src/fonts/**/*',
+  libs: 'src/libs/**/*'
+};
+ 
+function styles() {
+  return gulp.src('src/sass/style.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCSS())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('build/css'))
+    .pipe(bs.stream());
+}
+
+function html() {
+  return gulp.src('src/index.html')
+    .pipe(preprocess())
+    .pipe(gulp.dest('build'))
+    .pipe(bs.stream());
+}
+
+function scripts() {
+  return gulp.src(paths.js)
+    .pipe(concat('main.js'))
+    .pipe(terser())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('build/js'))
+    .pipe(bs.stream());
+}
+
+function images() {
+  return gulp.src(paths.img)
+    .pipe(gulp.dest('build/img'))
+    .pipe(bs.stream());
+}
+
+function fonts() {
+  return gulp.src(paths.fonts)
+    .pipe(gulp.dest('build/fonts'))
+    .pipe(bs.stream());
+}
+
+function libs() {
+  return gulp.src(paths.libs)
+    .pipe(gulp.dest('build/libs'))
+    .pipe(bs.stream());
+}
+
+function serve() {
+  bs.init({
+    server: {
+      baseDir: 'build'
+    }
+  });
+
+  gulp.watch(paths.html, html);
+  gulp.watch(paths.sass, styles);
+  gulp.watch(paths.js, scripts);
+  gulp.watch(paths.img, images);
+  gulp.watch(paths.fonts, fonts);
+  gulp.watch(paths.libs, libs);
+}
+
+const build = gulp.series(clean, gulp.parallel(styles, html, scripts, images, fonts, libs));
+const watch = gulp.series(build, serve);
+
+export {
+  clean,
+  styles,
+  html,
+  scripts,
+  images,
+  fonts,
+  libs,
+  watch,
+  serve
+};
+
+export default build;
